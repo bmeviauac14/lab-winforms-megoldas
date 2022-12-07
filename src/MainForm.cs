195a141e6f1121ac1f141a37ec94a470e925f5ba
Tóth Tibor <toth.tibor@autsoft.hu>
2022-12-07 16:26:38 +0100

    Anchor

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -11,5 +11,10 @@ namespace HelloWorldWF
         {
             tbDemoText.Text = "Hello";
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
