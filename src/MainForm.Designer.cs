2631f5dc3d65162c613ba33f1045604fa7c2698f
Tóth Tibor <toth.tibor@autsoft.hu>
2022-12-07 16:40:34 +0100

    logika

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -76,6 +76,9 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderName = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderSize = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderModified = new System.Windows.Forms.ColumnHeader();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.toolStripContainer1.BottomToolStripPanel.SuspendLayout();
@@ -493,15 +496,34 @@
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(266, 356);
             this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // listView1
             // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderSize,
+            this.columnHeaderModified});
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(530, 356);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Name";
+            // 
+            // columnHeaderSize
+            // 
+            this.columnHeaderSize.Text = "Size";
+            // 
+            // columnHeaderModified
+            // 
+            this.columnHeaderModified.Text = "Modified";
             // 
             // MainForm
             // 
@@ -512,6 +534,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "Hello World";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
@@ -580,5 +603,8 @@
         private SplitContainer splitContainer1;
         private TreeView treeView1;
         private ListView listView1;
+        private ColumnHeader columnHeaderName;
+        private ColumnHeader columnHeaderSize;
+        private ColumnHeader columnHeaderModified;
     }
 }
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -1,20 +1,84 @@
-namespace HelloWorldWF
+namespace HelloWorldWF;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    public MainForm()
     {
-        public MainForm()
+        InitializeComponent();
+    }
+
+    private void bUpdateText_Click(object sender, EventArgs e)
+    {
+        tbDemoText.Text = "Hello";
+    }
+
+    private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+        var root = treeView1.Nodes.Add("Local Disk (C:)");
+        root.Tag = new DirectoryInfo(@"C:\");
+        root.Nodes.Add("");
+    }
+
+    private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+    {
+        var parentDI = e.Node?.Tag as DirectoryInfo;
+        if (parentDI == null)
+            return;
+
+        e.Node?.Nodes.Clear();
+        try
         {
-            InitializeComponent();
+            foreach (var di in parentDI.GetDirectories())
+            {
+                var node = new TreeNode(di.Name);
+                node.Tag = di;
+                node.Nodes.Add("");
+                e.Node?.Nodes.Add(node);
+            }
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            // lenyeljük a hibát, így úgy fog tûnni, mintha üres lenne a mappa
+
+            // Opcionálisan feldobhatunk egy üzenetet a felhasználónak
+            // MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+    {
+        var parentDI = e.Node?.Tag as DirectoryInfo;
+        if (parentDI == null)
+            return;
+
+        listView1.Items.Clear();
+        try
+        {
+            foreach (FileInfo fi in parentDI.GetFiles())
+            {
+                listView1.Items.Add(
+                    new ListViewItem(new string[]
+                    {
+                        fi.Name,
+                        fi.Length.ToString(),
+                        fi.LastWriteTime.ToString(),
+                        fi.FullName
+                    }));
+            }
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            // lenyeljük a hibát, így úgy fog tûnni, mintha üres lenne a mappa
+            
+            // Opcionálisan feldobhatunk egy üzenetet a felhasználónak
+            // MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        private void bUpdateText_Click(object sender, EventArgs e)
-        {
-            tbDemoText.Text = "Hello";
-        }
-
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+        tbDemoText.Text = parentDI.FullName;
     }
 }
